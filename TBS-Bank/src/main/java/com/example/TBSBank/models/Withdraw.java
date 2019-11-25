0877f7ba536dbde4f16cb6bcce02d82fbf8611cb
ChristianNeal015 <cjdaking030@gmail.com>
2019-11-25 14:04:32 -0500

    Finished Withdraw/Controller/Service/Repository

--- TBS-Bank/src/main/java/com/example/TBSBank/models/Withdraw.java
+++ TBS-Bank/src/main/java/com/example/TBSBank/models/Withdraw.java
@@ -1,8 +1,97 @@
 package com.example.TBSBank.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Withdraw {
 
+    @Id
+    @GeneratedValue
+    @JsonProperty("id")
+    private Long id;
+    private String type;
+    private String transaction_date;
+    private String status;
+    private Long payerId;
+    private String medium;
+    private Double amount;
+    private String description;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTransaction_date() {
+        return transaction_date;
+    }
+
+    public void setTransaction_date(String transaction_date) {
+        this.transaction_date = transaction_date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getPayerId() {
+        return payerId;
+    }
+
+    public void setPayerId(Long payerId) {
+        this.payerId = payerId;
+    }
+
+    public String getMedium() {
+        return medium;
+    }
+
+    public void setMedium(String medium) {
+        this.medium = medium;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTODO() {
+        return TODO;
+    }
+
+    public void setTODO(String TODO) {
+        this.TODO = TODO;
+    }
 
     private String TODO;
       // id, type, transactionDate, status, payerId , medium , amount ,description
---
+++ TBS-Bank/src/main/java/com/example/TBSBank/repository/WithdrawRepository.java
@@ -0,0 +1,12 @@
+package com.example.TBSBank.repository;
+
+import com.example.TBSBank.models.Withdraw;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface WithdrawRepository extends CrudRepository<Withdraw, Long> {
+    List<Withdraw> findAllByPayerId(Long accountId);
+
+    List<Withdraw> findAllByAccountId(Long accountId);
+}
