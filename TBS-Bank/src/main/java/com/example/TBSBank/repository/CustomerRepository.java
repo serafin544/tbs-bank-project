c1d30136437489f25d0261ed9217d03f5299d358
Serafin Escobar <Serafin@JJs-MacBook-Pro.local>
2019-11-25 15:41:22 -0500

    fixedCustomer

--- TBS-Bank/src/main/java/com/example/TBSBank/repository/CustomerRepository.java
+++ TBS-Bank/src/main/java/com/example/TBSBank/repository/CustomerRepository.java
@@ -2,6 +2,7 @@ package com.example.TBSBank.repository;
 
 import com.example.TBSBank.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
 }
