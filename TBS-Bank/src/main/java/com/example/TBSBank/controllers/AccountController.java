b36210ab374808ec1e4637e1cba339d600fd1fca
rkoeller224 <xavierk123@gmail.com>
2019-11-27 11:18:40 -0500

    fuck this shit

--- TBS-Bank/src/main/java/com/example/TBSBank/controllers/AccountController.java
+++ TBS-Bank/src/main/java/com/example/TBSBank/controllers/AccountController.java
@@ -5,25 +5,21 @@ import com.example.TBSBank.models.Account;
 import com.example.TBSBank.services.AccountService;
 import com.example.TBSBank.services.CustomerService;
 import org.apache.coyote.Response;
-import org.omg.CORBA.PUBLIC_MEMBER;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
 
 @RestController
 public class AccountController {
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
 
     @Autowired
     AccountService accountService;
@@ -31,130 +27,82 @@ public class AccountController {
     @Autowired
     CustomerService customerService;
 
-
-//get all accounts
-        public Iterable<Account> getAllAccounts(Account account) {
-            List<Account> accounts = jdbcTemplate.query("select accountId,nickName,rewards,balance,customerId from account",(result,rowNum)-> new Account(result.getLong("accountId")
-                    ,result.getString("nickName")
-                    ,result.getInt("rewards")
-                    ,result.getDouble("balance")
-                    ,result.getLong("customerId"))
-                    );
-            return accounts;
+    @GetMapping( value = "/accounts")
+    public ResponseEntity<?> getAllAccounts(Account accounts){
+        List<Account> allAccounts = (List<Account>) accountService.getAllAccounts(accounts);
+        ResponseStatus rep = new ResponseStatus();
+        if(!allAccounts.isEmpty()){
+            rep.setCode(HttpStatus.OK.value());
+            rep.setData(allAccounts);
+            return new ResponseEntity<>(rep,HttpStatus.OK);
+        }else{
+            rep.setCode(HttpStatus.NOT_FOUND.value());
+            return new ResponseEntity<>(rep, HttpStatus.NOT_FOUND);
         }
-//get account by id
-        public Account getAccountById(Long accountId){
-            String query = "SELECT * FROM ACCOUNT WHERE ID = ?";
-            Account account = jdbcTemplate.queryForObject(query,new Object[]{accountId},
-                    new BeanPropertyRowMapper<>(Account.class));
-            return account;
+    }
 
+    @GetMapping( value = "/accounts/{id}")
+    public ResponseEntity<?> getAccountById(@PathVariable Long id){
+        ResponseStatus rep = new ResponseStatus();
+        Optional<Account> account = Optional.ofNullable(accountService.getAccountById(id));
+        if(account.isPresent()){
+            rep.setCode(HttpStatus.OK.value());
+            rep.setData(account);
+            return new ResponseEntity<>(rep, HttpStatus.OK);
+        }else{
+            rep.setCode(HttpStatus.NOT_FOUND.value());
+            return new ResponseEntity<>(rep,HttpStatus.NOT_FOUND);
         }
+    }
 
-//get account for customer
-    public Account getAccountForCustomer(Long customerId,Long accountId){
+    @GetMapping(value = "/customers/{id}/accounts")
+    public ResponseEntity<?> getAccountForCustomer(@PathVariable Long id){
 
-            String query = "Select ACCOUNT FOR CUSTOMER WHERE ID = ?";
-            Account account = jdbcTemplate.queryForObject(query,new Object[]{customerId,accountId},
-                    new BeanPropertyRowMapper<>(Account.class));
 
-            return account;
+
+        Optional<Account> CustomerAccounts = Optional.ofNullable(accountService.getAccountById(id));
+
+        ResponseStatus response = new ResponseStatus();
+        if(CustomerAccounts.isPresent()){
+            response.setCode(HttpStatus.OK.value());
+            response.setData(CustomerAccounts);
+            return new ResponseEntity<>(response,HttpStatus.OK);
+        }else{
+            response.setCode(HttpStatus.NOT_FOUND.value());
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping( value = "/customers/{id}/accounts")
+    public ResponseEntity<?> addAccount(@RequestBody Account account, @PathVariable Long id, @PathVariable String nickName,@PathVariable Integer rewards, @PathVariable Double balance,@PathVariable Long customerId)
+    {
+       int a = accountService.addAccount(id,account,nickName,rewards,balance,customerId);
+        if(a != 1){
+            HttpHeaders responseHeaders = new HttpHeaders();
+            URI newAcctUri = ServletUriComponentsBuilder
+                    .fromCurrentRequestUri()
+                    .path("/{id}")
+                    .buildAndExpand(a)
+                    .toUri();
+            responseHeaders.setLocation(newAcctUri);
+            return new ResponseEntity<>(null,responseHeaders,HttpStatus.CREATED);
+        }
+        else
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
 
     }
-//update account
-    public Account updateAccount(Long id, String nickName, Integer rewards, Double balance, Long customerId){
-            String query = "SELECT * FOR ACCOUNT WHERE ID = ?";
-            Account account = jdbcTemplate.queryForObject(query,new Object[]{id,nickName,rewards,balance,customerId},
-                    new BeanPropertyRowMapper<>(Account.class));
-            return account;
 
+    @PutMapping( value = "/accounts/{id}")
+    public ResponseEntity<?> updateAccount(@Valid @RequestBody Account account, @PathVariable Long accountId,@PathVariable String nickName,@PathVariable Integer rewards, @PathVariable Double balance,@PathVariable Long customerId) {
+      ResponseStatus responseStatus = new ResponseStatus();
+        accountService.updateAccount(account,accountId,nickName,rewards,balance,customerId);
+      return new ResponseEntity<>(HttpStatus.CREATED,HttpStatus.OK);
     }
-//add account
-    public int addAccount(Long id, String nickName, Integer rewards, Double balance, Long customerId){
-            String query = "INSERT INTO ACCOUNT VALUES(?,?,?)";
-            return jdbcTemplate.update(query,id,balance,customerId,rewards);
+
+    @DeleteMapping( value = "/accounts/{id}")
+    public ResponseEntity<?> delAccount(@RequestBody Long id){
+        Response response = new Response();
+        accountService.deleteAccount(id);
+        return new ResponseEntity<>(response,HttpStatus.OK);
     }
-//delete account
-    public int deleteAccount(Long id){
-            String query = "DELETE FROM ACCOUNT WHERE ID = ?";
-            return jdbcTemplate.update(query,id);
-    }
-//    @GetMapping( value = "/accounts")
-//    public ResponseEntity<?> getAllAccounts(Account accounts){
-//        List<Account> allAccounts = (List<Account>) accountService.getAllAccounts(accounts);
-//        ResponseStatus rep = new ResponseStatus();
-//        if(!allAccounts.isEmpty()){
-//            rep.setCode(HttpStatus.OK.value());
-//            rep.setData(allAccounts);
-//            return new ResponseEntity<>(rep,HttpStatus.OK);
-//        }else{
-//            rep.setCode(HttpStatus.NOT_FOUND.value());
-//            return new ResponseEntity<>(rep, HttpStatus.NOT_FOUND);
-//        }
-//    }
-
-//    @GetMapping( value = "/accounts/{id}")
-//    public ResponseEntity<?> getAccountById(@PathVariable Long id){
-//        ResponseStatus rep = new ResponseStatus();
-//        Optional<Account> acctt = accountService.getAccountById(id);
-//        if(acctt.isPresent()){
-//            rep.setCode(HttpStatus.OK.value());
-//            rep.setData(acctt);
-//            return new ResponseEntity<>(rep, HttpStatus.OK);
-//        }else{
-//            rep.setCode(HttpStatus.NOT_FOUND.value());
-//            return new ResponseEntity<>(rep,HttpStatus.NOT_FOUND);
-//        }
-//    }
-//
-//    @GetMapping(value = "/customers/{id}/accounts")
-//    public ResponseEntity<?> getAccountForCustomer(@PathVariable Long id){
-//
-//
-//
-//        Optional<Account> CustomerAccounts =  accountService.getAccountById(id);
-//
-//        ResponseStatus response = new ResponseStatus();
-//        if(CustomerAccounts.isPresent()){
-//            response.setCode(HttpStatus.OK.value());
-//            response.setData(CustomerAccounts);
-//            return new ResponseEntity<>(response,HttpStatus.OK);
-//        }else{
-//            response.setCode(HttpStatus.NOT_FOUND.value());
-//            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-//        }
-//    }
-
-//    @PostMapping( value = "/customers/{id}/accounts")
-//    public ResponseEntity<?> addAccount(@RequestBody Account account, @PathVariable Long id)
-//    {
-//        Account a = accountService.createAccountForCustomer(id, account);
-//        if(a != null){
-//            HttpHeaders responseHeaders = new HttpHeaders();
-//            URI newAcctUri = ServletUriComponentsBuilder
-//                    .fromCurrentRequestUri()
-//                    .path("/{id}")
-//                    .buildAndExpand(a.getId())
-//                    .toUri();
-//            responseHeaders.setLocation(newAcctUri);
-//            return new ResponseEntity<>(null,responseHeaders,HttpStatus.CREATED);
-//        }
-//        else
-//            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
-//
-//    }
-
-//    @PutMapping( value = "/accounts/{id}")
-//    public ResponseEntity<?> updateAccount(@Valid @RequestBody Account account, @PathVariable Long accountId) {
-//      ResponseStatus responseStatus = new ResponseStatus();
-//        accountService.updateAccount(account,accountId);
-//      return new ResponseEntity<>(HttpStatus.CREATED,HttpStatus.OK);
-//    }
-
-//    @DeleteMapping( value = "/accounts/{id}")
-//    public ResponseEntity<?> delAccount(@RequestBody Long id){
-//        Response response = new Response();
-//        accountService.deleteAccount(id);
-//        return new ResponseEntity<>(response,HttpStatus.OK);
-//    }
 }
--- TBS-Bank/src/main/java/com/example/TBSBank/services/AccountService.java
+++ TBS-Bank/src/main/java/com/example/TBSBank/services/AccountService.java
@@ -4,54 +4,115 @@ import com.example.TBSBank.exceptions.ResourceNotFoundException;
 import com.example.TBSBank.models.Account;
 import com.example.TBSBank.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.validation.Valid;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Optional;
 
+
 @Service
 public class AccountService {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Autowired
     AccountRepository accountRepository;
-//get all accounts
-    public Iterable<Account> getAllAccounts(Account accounts){
-        return accountRepository.findAll();
+    //get all accounts
+    public Iterable<Account> getAllAccounts(Account account) {
+        List<Account> acc = jdbcTemplate.query("id,nickName,rewards,balance,customer_id from account",(result,rowNum)-> new Account(result.getLong("accountId")
+                ,result.getString("nickName")
+                ,result.getInt("rewards")
+                ,result.getDouble("balance")
+                ,result.getLong("customerId"))
+        );
+        return acc;
+    }
+    //get account by id
+    public Account getAccountById(Long accountId){
+        String query = "SELECT * FROM ACCOUNT WHERE ID = ?";
+        Account acc = jdbcTemplate.queryForObject(query,new Object[]{accountId},
+                new BeanPropertyRowMapper<>(Account.class));
+        return acc;
+
     }
 
-//get all customer accounts
-    public List<Account> getAllCustomerAccounts(Long customerID) {
-        return accountRepository.findAllAccountsByCustomerId(customerID);
+    //get account for customer
+    public Account getAccountForCustomer(Long customerId,Long id){
+
+        String query = "Select ACCOUNT FOR CUSTOMER WHERE ID = ?";
+        Account acc = jdbcTemplate.queryForObject(query,new Object[]{customerId,id},
+                new BeanPropertyRowMapper<>(Account.class));
+
+        return acc;
+
     }
+    //update account
+    public Account updateAccount(@Valid Account account, Long id, String nickName, Integer rewards, Double balance, Long customerId){
+        String query = "SELECT * FOR ACCOUNT WHERE ID = ?";
+        Account acc = jdbcTemplate.queryForObject(query,new Object[]{id,nickName,rewards,balance,customerId},
+                new BeanPropertyRowMapper<>(Account.class));
+        return account;
 
-//get account by id
-
-    public Optional<Account> getAccountById(Long id){
-        return accountRepository.findById(id);
     }
-
-//create account for customer
-    public Account createAccountForCustomer(Long customerId, Account account) {
-        account.setCustomer(customerId);
-
-        return accountRepository.save(account);
+    //add account
+    public int addAccount(Long id, Account account, String nickName, Integer rewards, Double balance, Long customerId){
+        String query = "INSERT INTO ACCOUNT VALUES(?,?,?)";
+        return jdbcTemplate.update(query,id,balance,customerId,rewards,nickName);
     }
-
-//update customer
-    public Account updateAccount(Account account, Long accountId) {
-        account.setId(accountId);
-        return accountRepository.save(account);
+    //delete account
+    public int deleteAccount(Long id){
+        String query = "DELETE FROM ACCOUNT WHERE ID = ?";
+        return jdbcTemplate.update(query,id);
     }
-
-//delete account
-    public void deleteAccount(Long accountId) {
-
-        accountRepository.deleteById(accountId);
-    }
-
+////verify account
+//    public Account verifyAccountId(Long accountId, String s){
+//
+//        String query = "SELECT USERNAME FROM ACCOUNT WHERE ID = ?";
+//
+//    }
+////get all accounts
+//    public Iterable<Account> getAllAccounts(Account accounts){
+//        return accountRepository.findAll();
+//    }
+//
+////get all customer accounts
+//    public List<Account> getAllCustomerAccounts(Long customerID) {
+//        return accountRepository.findAllAccountsByCustomerId(customerID);
+//    }
+//
+////get account by id
+//
+//    public Optional<Account> getAccountById(Long id){
+//        return accountRepository.findById(id);
+//    }
+//
+////create account for customer
+//    public Account createAccountForCustomer(Long customerId, Account account) {
+//        account.setCustomer(customerId);
+//
+//        return accountRepository.save(account);
+//    }
+//
+////update customer
+//    public Account updateAccount(Account account, Long accountId) {
+//        account.setId(accountId);
+//        return accountRepository.save(account);
+//    }
+//
+////delete account
+//    public void deleteAccount(Long accountId) {
+//
+//        accountRepository.deleteById(accountId);
+//    }
+//
     //verify account
   public void verifyAccountId(Long accountId, String message) throws ResourceNotFoundException {
-    Optional<Account> account = getAccountById(accountId);
+    Optional<Account> account = Optional.ofNullable(getAccountById(accountId));
 
     if(!account.isPresent())
       throw new ResourceNotFoundException(message);
